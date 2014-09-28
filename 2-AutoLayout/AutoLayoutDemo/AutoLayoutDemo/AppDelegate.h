b749fe712bf8cd2039dcdf87977961ded3297a36
Jymn_Chen <guesn10000@126.com>
2014-09-28 23:17:29 +0800

    Initial Commit

---
+++ 2-AutoLayout/AutoLayoutDemo/AutoLayoutDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoLayoutDemo
+//
+//  Created by Jymn_Chen on 14-9-28.
+//  Copyright (c) 2014年 Jymn_Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 2-AutoLayout/AutoLayoutDemo/AutoLayoutDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AutoLayoutDemo
+//
+//  Created by Jymn_Chen on 14-9-28.
+//  Copyright (c) 2014年 Jymn_Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
