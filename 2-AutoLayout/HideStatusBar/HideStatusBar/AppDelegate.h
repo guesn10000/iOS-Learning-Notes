1cbb460320426d67f2e3edb4eec56f4545799858
Jymn_Chen <guesn10000@126.com>
2014-09-29 09:52:17 +0800

    Initial Commit

---
+++ 2-AutoLayout/HideStatusBar/HideStatusBar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HideStatusBar
+//
+//  Created by Jymn_Chen on 14-9-29.
+//  Copyright (c) 2014年 Jymn_Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 2-AutoLayout/HideStatusBar/HideStatusBar/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HideStatusBar
+//
+//  Created by Jymn_Chen on 14-9-29.
+//  Copyright (c) 2014年 Jymn_Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
